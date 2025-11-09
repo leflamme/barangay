ef9f9c7dc7ee1714cc9cab5791956e72b18e58c3
leflamme <lawrencejohnmhinanay@tua.edu.ph>
2025-11-09 21:18:19 +0800

    Add debug to conn

--- src/connection.php
+++ src/connection.php
@@ -1,4 +1,30 @@
 <?php 
+$host = getenv('MYSQLHOST'); 
+$db_name = getenv('MYSQLDATABASE'); 
+$username = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$port = getenv('MYSQLPORT');
+
+echo "Host: '" . $host . "'<br>";
+echo "DB Name: '" . $db_name . "'<br>";
+echo "User: '" . $username . "'<br>";
+echo "Password is set: " . (empty($password) ? "NO" : "YES") . "<br>";
+echo "Port: '" . $port . "'<br>";
+
+echo "--- ENDING DEBUG ---<br><br>";
+
+// Create connection using MySQLi
+$con = new mysqli($host, $username, $password, $db_name, $port);
+
+// Check connection
+if ($con->connect_error) {
+  die("Connection failed: " . $con->connect_error);
+} else {
+  echo "SUCCESS! Connected to the database.";
+}
+
+
+/* ----- 1st Railway SETUP -----
 // Read credentials from Environment Variables provided by Railway
 $host = getenv('MYSQLHOST'); 
 $db_name = getenv('MYSQLDATABASE'); 
@@ -13,8 +39,9 @@ $con = new mysqli($host, $username, $password, $db_name, $port);
 if ($con->connect_error) {
   die("Connection failed: " . $con->connect_error);
 }
+*/
 
-/*
+/* ----- Local Docker SETUP -----
 // The host is the service name from docker-compose.yml
 $host = 'db'; 
 
