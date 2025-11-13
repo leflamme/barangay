67d52c069131332996fbbfc141f9b5e2e719a401
leflamme <lawrencejohnmhinanay@tua.edu.ph>
2025-11-13 14:46:24 +0800

    Fix: Removed User Photo and Replaced with Logo

--- src/secretary/myProfile.php
+++ src/secretary/myProfile.php
@@ -230,30 +230,11 @@ try{
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4 sidebar-no-expand">
     <!-- Brand Logo -->
-    <a href="#" class="brand-link text-center">
-    <?php 
-        if($image != '' || $image != null || !empty($image)){
-          echo '<img src="'.$image_path.'" id="logo_image" class="img-circle elevation-5 img-bordered-sm" alt="logo" style="width: 70%;">';
-        }else{
-          echo ' <img src="../assets//logo//logo.png" id="logo_image" class="img-circle elevation-5 img-bordered-sm" alt="logo" style="width: 70%;">';
-        }
-
-      ?>
-      <span class="brand-text font-weight-light"></span>
-    </a>
+    <img src="../assets/logo/ksugan.jpg" alt="">
 
     <!-- Sidebar -->
     <div class="sidebar">
     
-
-    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="../assets/dist/img/logo.png" class="img-circle elevation-5 img-bordered-sm" alt="User Image">
-        </div>
-        <div class="info text-center">
-          <a href="#" class="d-block text-bold"><?= strtoupper($user_type) ?></a>
-        </div>
-      </div>
       <!-- Sidebar Menu -->
       <nav class="mt-2">
       <ul class="nav nav-pills nav-sidebar flex-column nav-child-indent" data-widget="treeview" role="menu" data-accordion="false">
