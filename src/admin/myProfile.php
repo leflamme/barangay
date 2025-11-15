964babc03369eb569a960cec040a72e3843eafa2
leflamme <lawrencejohnmhinanay@tua.edu.ph>
2025-11-16 01:38:29 +0800

    add title and web icon

--- src/admin/myProfile.php
+++ src/admin/myProfile.php
@@ -54,7 +54,9 @@ try{
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title></title>
+  <title>Admin Profile</title>
+  <!-- Website Logo -->
+  <link rel="icon" type="image/png" href="../assets/logo/ksugan.jpg">
  
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="../assets/plugins/fontawesome-free/css/all.min.css">
