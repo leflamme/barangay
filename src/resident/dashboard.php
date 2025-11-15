1f2c21e3bc936e33a056f09343309cf7b09c1a7e
leflamme <lawrencejohnmhinanay@tua.edu.ph>
2025-11-15 15:11:51 +0800

    revised sidebar

--- src/resident/dashboard.php
+++ src/resident/dashboard.php
@@ -169,10 +169,10 @@ body, .wrapper, .content-wrapper {
       <ul class="nav nav-pills nav-sidebar flex-column nav-child-indent" data-widget="treeview" role="menu" data-accordion="false">
         <li class="nav-item"><a href="dashboard.php" class="nav-link active"><i class="nav-icon fas fa-tachometer-alt"></i><p>Dashboard</p></a></li>
         <li class="nav-item"><a href="personalInformation.php" class="nav-link"><i class="nav-icon fas fa-id-card"></i><p>Personal Information</p></a></li>
-        <li class="nav-item"><a href="myRecord.php" class="nav-link"><i class="nav-icon fas fa-user-tie"></i><p>Blotter Record</p></a></li>
         <li class="nav-item"><a href="drrmPlan.php" class="nav-link"><i class="fas fa-clipboard-list nav-icon text-red"></i><p>Emergency Plan</p></a></li>
-        <li class="nav-item"><a href="changePassword.php" class="nav-link"><i class="nav-icon fas fa-lock"></i><p>Change Password</p></a></li>
+        <li class="nav-item"><a href="myRecord.php" class="nav-link"><i class="nav-icon fas fa-user-tie"></i><p>Blotter Record</p></a></li>
         <li class="nav-item"><a href="certificate.php" class="nav-link"><i class="nav-icon fas fa-tachometer-alt"></i><p>Certificate</p></a></li>
+        <li class="nav-item"><a href="changePassword.php" class="nav-link"><i class="nav-icon fas fa-lock"></i><p>Change Password</p></a></li>
       </ul>
     </nav>
   </div>
